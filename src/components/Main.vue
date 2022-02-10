8dca41ecd072b8c71d55d635ab54e2a45b6939e8
Immolazione <federico.deered@gmail.com>
2022-02-10 17:37:54 +0100

    fix cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,8 @@
 <template>
 <main class="vh-100">
     <div class="container">
-        <section id="cards" class="d-flex flex-wrap row g-0">
-            <div class="mx-3 single-card d-flex flex-column" v-for="(card, index) in songsList" :key="index">
+        <section id="cards" class="d-flex flex-wrap row row-cols-6">
+            <div class="single-card d-flex flex-column col mx-2" v-for="(card, index) in songsList" :key="index">
                 <img class="px-4 pt-4" :src="card.poster" :alt="card.title">
                 <h1 id="title" class="text-white text-center p-2">{{card.title}}</h1>
                 <h5 id="author" class="text-center px-2 mb-0">{{card.author}}</h5>
@@ -38,13 +38,14 @@ main {
 }
 #cards{
     padding-top: 6rem;
+    justify-content: space-between;
     .single-card{
-        width: 10rem;
-        height: 20rem;
+        // width: 10rem;
+        height: 22rem;
         background-color: $header-color;
         margin-top: 2rem;
         img {
-            max-width: 100%;
+            width: 100%;
         }
         #title {
             font-size: 1.5rem;
